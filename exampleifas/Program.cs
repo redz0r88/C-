b2e91722345a9c4c704112b62cbe2ef39b536d2b
Alexandr <alexandr.grachyov@gmail.com>
2023-07-05 21:18:42 +0300

    привет Маша

---
+++ exampleifas/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+Console.WriteLine("Ура, это же МАША");    
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
