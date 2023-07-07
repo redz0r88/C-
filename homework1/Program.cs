0e15f4ef6fc3029170fc66300fa80820218ef51b
Alexandr <alexandr.grachyov@gmail.com>
2023-07-07 22:49:46 +0300

    Задача 2 из домашней работы

---
+++ homework1/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.Clear();
+Console.WriteLine("Введите числа");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+if (b > max) 
+max = b;
+
+Console.WriteLine("Максимальное" + " " + max);
