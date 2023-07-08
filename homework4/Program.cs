d8f30b8e09e5237dfdc798d5b6bae556166b3905
Alexandr <alexandr.grachyov@gmail.com>
2023-07-08 09:28:07 +0300

    Я сделал даже последнее задание

---
+++ homework4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Напишите любое число");
+int a = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+
+    while (i <= a)
+    {
+    if (i % 2 == 0)
+    Console.WriteLine(i + " ");
+    i++;
+    }
