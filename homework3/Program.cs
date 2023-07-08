2cae62abdbcef476f7f2ce2cf05592a5c7e628c2
Alexandr <alexandr.grachyov@gmail.com>
2023-07-08 08:57:04 +0300

    Сделал задачу номер 6

---
+++ homework3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("Четное число");
+}
+else 
+{
+    Console.WriteLine("Нечетное число");
+}
