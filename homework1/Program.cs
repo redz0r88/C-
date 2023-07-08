19d4235e3225e92be3e2870e925cef41e0d4eabd
Alexandr <alexandr.grachyov@gmail.com>
2023-07-08 08:32:14 +0300

    После многих попыток я выолнил первое дз

--- homework1/Program.cs
+++ homework1/Program.cs
@@ -4,9 +4,17 @@ Console.WriteLine("Введите числа");
 
 int a = Convert.ToInt32(Console.ReadLine());
 int b = Convert.ToInt32(Console.ReadLine());
-int max = a;
+int max = 0;
+int min = 0;
 
-if (b > max) 
-max = b;
+if (a > b)
+{
+max = a; min = b;
+}
+else
+{
+    max = b; min = a;
+}
 
 Console.WriteLine("Максимальное" + " " + max);
+Console.WriteLine("Минимальное" + " " + min);
