62714dac0659809bc21f647e3bdf35c48b373684
Alexandr <alexandr.grachyov@gmail.com>
2023-07-08 20:27:29 +0300

    сделал задачу 10

---
+++ homework5/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Напишите трёхзначное число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n > 99 && n < 1000)
+{
+    int n1 = (n % 100 - n % 10) / 10;
+    Console.WriteLine($"Вторая цифра числа {n} является {n1}");
+}
