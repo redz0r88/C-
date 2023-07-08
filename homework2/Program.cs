6333f1255d641a7e527cbe3160c1e49ef2fc6f76
Alexandr <alexandr.grachyov@gmail.com>
2023-07-08 08:44:52 +0300

    Сделал задачу номер 4

---
+++ homework2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+int max = 0;
+Console.Write("Введите числа");
+int FirstNumber = Convert.ToInt32(Console.ReadLine());
+int SecondNumber = Convert.ToInt32(Console.ReadLine());
+int ThirdNumber = Convert.ToInt32(Console.ReadLine());
+
+if (FirstNumber > max)
+{
+    max = FirstNumber;
+}
+if (SecondNumber > max)
+{
+max = SecondNumber;
+}
+if (ThirdNumber > max)
+{
+max = ThirdNumber;
+}
+Console.WriteLine("Максимальное значение" + " " + "=" + " " + max);
