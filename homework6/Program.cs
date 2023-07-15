3381dffbbd6b553bdd61c35f9a76a587413df07a
Alexandr <alexandr.grachyov@gmail.com>
2023-07-15 10:56:41 +0300

    Задача 13

---
+++ homework6/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+int ThirdDigit(int number)
+{
+    int result = -1;
+    if (number >=100)
+    {
+     while (number > 999)
+     {
+        number = number / 10;
+     }
+        result = number % 10;
+    }
+return result; // [B]имя "result" не существует в текущем контексте[/B]
+}
+ 
+Console.Write("Введите трёхзначное число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+ 
+if (ThirdDigit(number1) == -1)
+Console.WriteLine("третьей цифры нет");
+else
+Console.WriteLine($"Third digit is {ThirdDigit(number1)}");
