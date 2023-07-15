8d66b046d107947c9cc8b85df5334e9d5b9f75ec
Alexandr <alexandr.grachyov@gmail.com>
2023-07-15 11:08:32 +0300

    Домашнее задание 15

---
+++ homework7/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+int Prompt(string msg)
+{
+    Console.WriteLine($"{msg}");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int number = Prompt("Введите число");
+string ss = "Это не день недели";
+string check(int number)
+{
+    if (number >= 6 && number < 8)
+    ss = "Это выходной";
+    if (number >= 1 && number < 6)
+    ss = "Это будний день";
+    return ss;
+}
+
+Console.WriteLine(check(number));
