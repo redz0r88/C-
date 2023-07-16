075708fadca1bc6e491d02acb0e6454cf269425b
Alexandr <alexandr.grachyov@gmail.com>
2023-07-16 11:41:49 +0300

    Домашняя работа23

---
+++ homework10/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Таблица кубов: ");
+for (int i=1;i<=n;i++)
+{
+int k = i*i*i;
+Console.Write($"{k}, ");
+}
