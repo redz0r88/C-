07ffc97157c1bd4945fcb7916ae0302a9df14121
Alexandr <alexandr.grachyov@gmail.com>
2023-08-04 14:29:07 +0300

    задача 68 аттестация

---
+++ test3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+int Prompt(string message)
+{
+  Console.Write(message);
+  int result = Convert.ToInt32(Console.ReadLine());
+  return result;
+}
+
+int Akkerman(int m, int n)
+{
+  if (m == 0) return n + 1;
+  if (m > 0 && n == 0) return Akkerman(m - 1, 1);
+  else return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+
+int m = Prompt("Input M: ");
+int n = Prompt("Input N: ");
+
+Console.WriteLine($"A({m},{n}) = {Akkerman(m, n)}");
