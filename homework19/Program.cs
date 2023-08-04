c7c142815194f614b5573c5344df0c36bf6f25ee
Alexandr <alexandr.grachyov@gmail.com>
2023-08-04 13:43:15 +0300

    Домашняя работа 47

---
+++ homework19/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+Console.Write("Введите размерность m массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите размерность n массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] randomArray = new double[m,n];
+
+void mas(int m, int n)
+{
+int i,j;
+Random rand = new Random();
+for (i = 0; i < m; i++)
+{
+Console.WriteLine();
+for (j = 0; j < n; j++)
+{
+randomArray[i,j] = rand.NextDouble();
+Console.Write($"{randomArray[i,j]:F2} ");
+}
+
+}
+
+}
+
+mas(m,n);
