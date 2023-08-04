6f4b31b7d88c2074129486574cd035a721fd36c8
Alexandr <alexandr.grachyov@gmail.com>
2023-08-04 14:27:11 +0300

    задача 66 аттестация

---
+++ test2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+int Prompt(string message)
+{
+  Console.Write(message);
+  int result = Convert.ToInt32(Console.ReadLine());
+  return result;
+}
+
+int SumOfElements(int n, int m)
+{
+  if (n == m) return n;
+  else return SumOfElements(n + 1, m) + n;
+}
+
+int n = Prompt("Input N: ");
+int m = Prompt("Input M: ");
+
+Console.WriteLine(SumOfElements(n, m));
