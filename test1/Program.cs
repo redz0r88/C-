29420d75c149a6e0d1bd0fd5176514380692abd8
Alexandr <alexandr.grachyov@gmail.com>
2023-08-04 14:24:06 +0300

    задача 64 аттестация

---
+++ test1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+int Prompt(string message)
+{
+  Console.Write(message);
+  int result = Convert.ToInt32(Console.ReadLine());
+  return result;
+}
+
+int NaturalNumber(int n, int m)
+{
+  if (n == m) return n;
+  else Console.Write($"{NaturalNumber(n, m + 1)}, ");
+  return m;
+}
+
+int n = Prompt("Input N: ");
+if (n < 1)
+{
+  Console.WriteLine("Ввдите положительное число!");
+  return;
+}
+Console.WriteLine(NaturalNumber(n, 1));
